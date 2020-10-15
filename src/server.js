fccdfb1d7d511fe4c0947927276dab7827e2db11
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-10-15 17:35:13 -0300

    instalando Npm e Node.js Pt:2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('teste')
+// importar dependencia
+const express = require('express');
+const path = require('path');
+
+//iniciar o express
+const server = express()
+server
+//utilizando os arquivos estaticos
+.use(express.static('public'))
+
+//criar uma rota 
+server.get('/', (request,response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//ligar o servidor
+server.listen(5500)
